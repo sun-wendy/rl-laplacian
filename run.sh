44022deb7d9c93c4855826a3e932b13e7db94acf
jonathan <jonathancolacocarr@gmail.com>
2024-07-15 17:43:20 -0400

    Add evaluation video

--- run.sh
+++ run.sh
@@ -10,18 +10,18 @@ for seed in {1..5}
 do
   # Save a video of the agent on the first run
   python3 run_experiment.py \
-      --env_name="four_rooms" \
+      --env_name="four_rooms_alt_with_vases" \
       --n_episodes=500 \
       --max_steps=100 \
       --seed=$seed \
       --agent_class="QLearningAgent" \
-      --n_eigenoptions=32 \
+      --n_eigenoptions=0 \
       --diffusion="None" \
       --discount=0.9 \
       --mode=save \
       --log_dir=./logs/$current_datetime \
-      --suffix="new" \
+      --suffix="" \
       --wandb=True \
       --wandb_project=side-effects-debug\
-      --eval_video=False
+      --eval_video=True
 done
